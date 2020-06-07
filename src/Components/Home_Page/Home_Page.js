609bc433cc71b12242b7181bbe3b13b4785c6cb0
Naveen Kumar <naveen.abiabi@gmail.com>
2020-06-07 21:31:11 +0530

    feat: Added home page

--- src/Components/Home_Page/Home_Page.js
+++ src/Components/Home_Page/Home_Page.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function HomePage() {
     return (
-        <div>
-            hello
+        <div className="home-page">
+            Welcome to Built In Studio
         </div>
     )
 }
