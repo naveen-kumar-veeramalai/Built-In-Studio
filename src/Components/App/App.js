fe11e5438f69486a2ed721ee4bf5067d611f44e9
Naveen Kumar <naveen.abiabi@gmail.com>
2020-06-08 09:51:07 +0530

    fix: change in class name and styles

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-content">
       <Router>
         <Switch>
           <Route path="/" exact component={HomePage} />
